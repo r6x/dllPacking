3b4189eb7f9077f062a3715dfda41af45f0d2bfe
r6x <44950886+r6x@users.noreply.github.com>
2021-10-19 18:29:07 +0300

    Init commit

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,39 @@
+﻿
+#include <iostream>
+#include <windows.h>
+#include <fcntl.h>
+#include <io.h>
+
+#include "MemoryModule.h"
+
+using namespace std;
+int main()
+{
+    typedef void(*myfunc) ();
+    HRSRC hRes = FindResource(NULL, TEXT("SIMPLEDLL"), RT_RCDATA);
+    if (hRes == NULL) {
+        cout << "Resource was not found" << endl;
+    }
+    else
+    {
+        HGLOBAL hGlobal = NULL;
+        hGlobal = LoadResource(NULL, hRes);
+        if (hGlobal != NULL) {
+            DWORD dwFileSize = SizeofResource(NULL, hRes);
+            HMEMORYMODULE handle = MemoryLoadLibrary(hGlobal, dwFileSize);
+            if (handle != NULL) {
+                myfunc func = (myfunc)MemoryGetProcAddress(handle, "func");
+                if (func != NULL) {
+                    func();
+                }
+                else {
+                    cout << "functoin is null" << endl;
+                }
+            }
+            else {
+                cout << "handle is null" << endl;
+            }  
+        }
+    }
+    system("pause");
+}
